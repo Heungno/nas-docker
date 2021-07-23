a8e2073fc991fa25f532fa8ae3a57a4bc835479f
heungno <gmd5366@gmail.com>
2021-07-23 09:32:44 +0900

    경로 변경

--- certbot/cert-renew.sh
+++ certbot/cert-renew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 인증서 갱신 
 docker run --rm --name certbot \
--v '/share/homes/docker/certbot/conf:/etc/letsencrypt' \
--v '/share/homes/docker/certbot/logs:/var/log/letsencrypt' \
--v '/share/homes/docker/certbot/data:/var/www/certbot' \
-certbot/certbot renew --server https://acme-v02.api.letsencrypt.org/directory --cert-name {{domain.com}}
+-v './conf:/etc/letsencrypt' \
+-v './logs:/var/log/letsencrypt' \
+-v './data:/var/www/certbot' \
+certbot/certbot renew --server https://acme-v02.api.letsencrypt.org/directory --cert-nam.
